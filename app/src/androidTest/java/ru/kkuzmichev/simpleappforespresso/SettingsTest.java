9b3daa4263881f9914416425823006c2e08fca47
Slava netologia <s-29@mail.ru>
2024-12-24 12:23:02 +0300

    2.6 Task 1

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/SettingsTest.java
@@ -0,0 +1,56 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.Matchers.allOf;
+
+import android.content.Intent;
+import android.net.Uri;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SettingsTest {
+    //@Rule ... Если вы решили реализовать проверку с использование «Правила»
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void testName() {
+        Intents.init();
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        ViewInteraction settings = onView(
+                withText("Settings")
+        );
+        settings.check(
+                matches(isDisplayed()));
+
+        settings.perform(click());
+        intended (allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData(Uri.parse("https://google.com"))
+                )
+        );
+        Intents.release();
+        //Проверяем intent, он должен передавать url и action
+        //Intents..
+    }
+}
